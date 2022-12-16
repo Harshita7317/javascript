a917213310e610b5178b82e0fb8fda863abbb00b
Harshita7317 <118048036+Harshita7317@users.noreply.github.com>
2022-12-16 11:07:59 +0530

    Add files via upload

---
+++ forloops_practice.js
@@ -0,0 +1,40 @@
+//1- Table of 11 using for loop
+/*let a=11;
+for(let i=1;i<11;i++)
+{
+let table= a*i;
+console.log(table);
+} */
+
+//2- Factors of a number.eg- factors of 10 are 1,2,5,10.
+/*let b=10;
+for(let i=1;i<=10;i++)
+{
+   if(b%i==0){
+   console.log(i);
+   }   
+}*/
+ //3 Sum of first 10 natural numbers.
+ /*let Sum=0; 
+ for(let i=1;i<11;i++)
+  {
+    Sum=Sum+i;
+    console.log(Sum);
+  }
+  */
+
+  //4-Program to find the square of first 10 natural numbers.
+  let square;
+  for(let i=1;i<11;i++)
+  {
+    square=i*i;
+    console.log(square);
+  }
+
+  //5- Cube of first 10 natural numbers.
+  let cube;
+  for(let  i=1;i<=10;i++)
+  {
+    cube=i*i*i;
+    console.log(cube);
+  }
